e9617740c9053a6287b9a9cd52ab1b48deb836c1
susu223344 <126578661+susu223344@users.noreply.github.com>
2023-10-01 19:40:58 +0800

    Add files via upload

--- App.py
+++ App.py
@@ -13,16 +13,16 @@ else:
     model1 = st.session_state["model1"]
     model2 = st.session_state["model2"]
 scaler = joblib.load("Scaler.pkl")
-continuous_vars = ['age', 'aniongap', 'resp_rate', 'apsiii']
+continuous_vars = ['Age','Anion gap','Respiratory rate','APSIII']
 
 st.set_page_config(layout='wide')
 
-st.write("<h1 style='text-align: center'>Analysis of device-associated infections and survival</h1>",
+st.write("<h1 style='text-align: center'>Prediction for Device-Associated Infections and 30-Day Survival Outcome of Intensive Care Unit Patients Undergoing Invasive Device Procedures</h1>",
          unsafe_allow_html=True)
-
+st.markdown('-----')
 dic1 = {
-    '男': 1,
-    '女': 0
+    'Male': 1,
+    'Female': 0
 }
 dic2 = {
     'Yes': 1,
@@ -31,30 +31,30 @@ dic2 = {
 with st.sidebar:
     st.markdown("# Input variable")
     st.markdown('-----')
-    mv = st.selectbox("mv", ["Yes", "No"])
-    uc = st.selectbox("uc", ["Yes", "No"])
-    cvc = st.selectbox("cvc", ["Yes", "No"])
-    age = st.text_input("age")
-    gender = st.selectbox("gender", ["男", "女"])
-    aniongap = st.text_input("aniongap")
-    resp_rate = st.text_input("resp_rate")
-    liver_disease = st.selectbox("liver_disease", ["Yes", "No"])
-    myocardial_infarct = st.selectbox("myocardial_infarct", ["Yes", "No"])
-    malignant_cancer = st.selectbox("malignant_cancer", ["Yes", "No"])
-    tracheostomy = st.selectbox("tracheostomy", ["Yes", "No"])
-    apsiii = st.text_input("apsiii")
-    race_black = st.selectbox("race_black", ["Yes", "No"])
-    case_micu_sicu = st.selectbox("case_micu/sicu", ["Yes", "No"])
+    IMV = st.selectbox("Invasive mechanical ventilation (IMV)", ["Yes", "No"])
+    IUC = st.selectbox("Indwelling urinary catheter (IUC)", ["Yes", "No"])
+    CVC = st.selectbox("Central venous catheter (CVC)", ["Yes", "No"])
+    Age = st.text_input("Age (years)")
+    Gender = st.selectbox("Gender", ["Male", "Female"])
+    Anion_gap = st.text_input("Anion gap (mmol/L)")
+    Respiratory_rate = st.text_input("Respiratory rate (min)")
+    Liver_disease = st.selectbox("Liver disease", ["Yes", "No"])
+    Myocardial_infarct = st.selectbox("Myocardial infarct", ["Yes", "No"])
+    Malignant_cancer = st.selectbox("Malignant cancer", ["Yes", "No"])
+    Tracheostomy = st.selectbox("Tracheostomy", ["Yes", "No"])
+    APSIII = st.text_input("APSIII")
+    Black = st.selectbox("Black", ["Yes", "No"])
+    MICU_SICU = st.selectbox("Medical or surgical intensive care unit (MICU/SICU)", ["Yes", "No"])
 if st.sidebar.button("Predict"):
     with st.spinner("Forecast, please wait..."):
-        test_df = pd.DataFrame([dic2[mv], dic2[uc], dic2[cvc], dic1[gender],
-                                dic2[liver_disease], dic2[myocardial_infarct], dic2[malignant_cancer],
-                                dic2[case_micu_sicu]],
-                               index=['mv', 'uc', 'cvc', 'gender', 'liver_disease', 'myocardial_infarct',
-                                      'malignant_cancer', 'case_micu/sicu']).T
+        st.header('1. Prediction for device-associated infections')
+        test_df = pd.DataFrame([dic2[IMV], dic2[IUC], dic2[CVC], dic1[Gender],
+                                dic2[Liver_disease], dic2[Myocardial_infarct], dic2[Malignant_cancer],
+                                dic2[MICU_SICU]],
+                               index=['IMV', 'IUC', 'CVC', 'Gender', 'Liver disease', 'Myocardial infarct',
+                                      'Malignant cancer', 'MICU/SICU']).T
         explainer = shap.Explainer(model1)  # 创建解释器
         shap_ = explainer.shap_values(test_df)
-
         shap.waterfall_plot(
             shap.Explanation(values=shap_[0, :], base_values=explainer.expected_value, data=test_df.iloc[0, :]),
             show=False)
@@ -62,7 +62,6 @@ if st.sidebar.button("Predict"):
         plt.savefig('shap1.png', dpi=300)
         col1, col2, col3 = st.columns([2, 5, 3])
         shap.initjs()
-
         shap.force_plot(explainer.expected_value, shap_[0, :], test_df.iloc[0, :], show=False, matplotlib=True,
                         figsize=(20, 5))
         plt.xticks(fontproperties='Times New Roman', size=16)
@@ -72,22 +71,23 @@ if st.sidebar.button("Predict"):
         with col2:
             st.image('shap1.png')
         st.image('shap2.png')
-        st.success("Probability: {:.3f}%".format(model1.predict_proba(test_df)[:, 1][0] * 100))
-        st.markdown('----')
+        st.success("Probability of device-associated infection: {:.3f}%".format(model1.predict_proba(test_df)[:, 1][0] * 100))
+        st.header('2. 30-day Kaplan-Meier survival curve')
+        
         test_df2 = pd.DataFrame(
-            [dic2[mv], float(age), float(aniongap), float(resp_rate), dic2[tracheostomy], float(apsiii),
-             dic2[race_black],
-             dic2[case_micu_sicu]],
-            index=['mv', 'age', 'aniongap', 'resp_rate', 'tracheostomy', 'apsiii',
-                   'race_black', 'case_micu/sicu']).T
+            [dic2[IMV], float(Age), float(Anion_gap), float(Respiratory_rate), dic2[Tracheostomy], float(APSIII),
+             dic2[Black],
+             dic2[MICU_SICU]],
+            index=['IMV', 'Age', 'Anion gap', 'Respiratory rate', 'Tracheostomy', 'APSIII',
+                   'Black', 'MICU/SICU']).T
         test_df2[continuous_vars] = scaler.transform(test_df2[continuous_vars])
         surv_funcs = model2.predict_survival_function(test_df2)
         col4, col5, col6 = st.columns([2, 5, 3])
         fig, ax = plt.subplots()
         for fn in surv_funcs:
             ax.step(fn.x, fn(fn.x), where="post", color="#8dd3c7", lw=2)
-        plt.ylabel("est. probability of survival $\hat{S}(t)$")
-        plt.xlabel("time $days$")
+        plt.ylabel("Probability of survival (%)")
+        plt.xlabel("Time since received the first invasive procedure (days)")
         plt.grid()
         with col5:
             st.pyplot(fig)
